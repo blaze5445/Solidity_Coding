9eeb2e2a169efebaf73bb745a50c32a707b8e78c
blaze5445 <blaze5@tutamail.com>
2024-08-02 15:06:13 +0300

    add new styles

---
+++ ITeamService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICD.Base.BusinessServiceContract
+{
+    public interface ITeamService
+    {
+        Task<InsertTeamResult> InsertTeamAsync(InsertTeamRequest request);
+        Task<RemoveTeamFromChampionshipResult> RemoveTeamFromChampionshipAsync(RemoveTeamFromChampionshipRequest request);
+        Task<GetTeamsBySpecificChampionshipResult> GetTeamsBySpecificChampionshipAsync(GetTeamsBySpecificChampionshipQuery query);
+    }
+}
